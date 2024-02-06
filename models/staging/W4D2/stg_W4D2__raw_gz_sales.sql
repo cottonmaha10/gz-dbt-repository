1bf242c8a0b0e684038dfb47d05caaeb69ee57f5
Maha Paramjothy <mahaletchumi.paramjothy@gmail.com>
2024-02-06 11:58:33 +0000

    rename files

--- models/staging/W4D2/stg_W4D2__raw_gz_sales.sql
+++ models/staging/W4D2/stg_W4D2__raw_gz_sales.sql
@@ -1,22 +1,12 @@
-with 
+with
 
-source as (
+    source as (select * from {{ source("W4D2", "raw_gz_sales") }}),
 
-    select * from {{ source('W4D2', 'raw_gz_sales') }}
+    renamed as (
 
-),
+        select date_date, orders_id, pdt_id as products_id, revenue, quantity from source
 
-renamed as (
+    )
 
-    select
-        date_date,
-        orders_id,
-        pdt_id,
-        revenue,
-        quantity
-
-    from source
-
-)
-
-select * from renamed
+select *
+from renamed
