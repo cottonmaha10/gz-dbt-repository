07a12bec65e65028abee0e5233901c176df3a768
Maha Paramjothy <mahaletchumi.paramjothy@gmail.com>
2024-02-06 17:36:47 +0000

    UPDATE orders_margin

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -2,12 +2,15 @@ WITH sqa AS (SELECT
 date_date
 ,count (orders_id)AS nb_orders
 ,ROUND (SUM (MARGIN),2) AS total_margin
+,ROUND (SUM (revenue),2) as total_revenue
 
 from {{ref("int_sales_margin")}}
 GROUP BY 1)
 
-SELECT 
-date_date
+SELECT *
+
 ,ROUND (total_margin/nb_orders,2) AS margin_per_order
+,ROUND (total_revenue/nb_orders,2) AS average_basket
 from sqa
+ORDER BY date_date
 --CALCULATE MARGIN PER ORDER
